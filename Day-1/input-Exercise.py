19295e967c6f51476319f4e8747e69e3dcd2f857
Muhammad Faisal <beingfaisalmalik@gmail.com>
2023-07-27 11:00:33 -0700

    Done With Day 1
    Completed All Of The Exercises, Projects, Tutorials

--- Day-1/input-Exercise.py
+++ Day-1/input-Exercise.py
@@ -1,1 +1,2 @@
+#Here The Concept of The Function Chaining Is Used First Input Then Length Then The Print Function
 print(len(input("What is your name? ")))
---
+++ Day-1/variables.py
@@ -0,0 +1,5 @@
+name = input("What Is Your Name: \n") # For Refering The Name In Future To Use It
+length = len(name)
+print("Hey The Length Of Your Name Is:\n")
+print(length)
+

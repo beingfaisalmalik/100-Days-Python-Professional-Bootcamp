ad5a8547dec4014bf5ce67f21121ed9519b224c5
Muhammad Faisal <beingfaisalmalik@gmail.com>
2023-10-04 01:27:42 -0700

    Day 36 Completed

--- Day-35/weatherdata.py
+++ Day-35/weatherdata.py
@@ -5,10 +5,6 @@ response = requests.get('https://api.openweathermap.org/data/2.5/weather?q=karac
 # response = requests.get('https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=24.9056&lon=67.0822&appid=dbd3b02d8958d62185d02e944cd5f522')
 flag = response.json()['weather']
 
-
-
-
-
 smtp_server = "smtp.gmail.com"
 port = 587  # For starttls
 sender_email = "faisalmalikff.99@gmail.com"
---
+++ Day-36/Stock-Alert.py
@@ -0,0 +1,47 @@
+import requests
+import smtplib, ssl
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+urlstock = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=GQXW7I3YRQD39KG3'
+urlnews = ('https://newsapi.org/v2/everything?q=ibm&apiKey=79792340121241d7a0f42f1e7b68a0de')
+r = requests.get(urlstock)
+response = requests.get(urlnews)
+data = r.json()
+
+final_price = data['Global Quote']['05. price']
+high_price = data['Global Quote']['03. high']
+low_price = data['Global Quote']['04. low']
+change_per = data['Global Quote']['10. change percent']
+headline = response.json()['articles'][0]['title']
+Brief = response.json()['articles'][0]['description']
+news = None
+
+
+print(news)
+
+smtp_server = "smtp.gmail.com"
+port = 587  # For starttls
+sender_email = "faisalmalikff.99@gmail.com"
+password = 'kzknhrovilvqdoti'
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+# Try to log in to server and send email
+try:
+    server = smtplib.SMTP(smtp_server,port)
+    server.ehlo() # Can be omitted
+    server.starttls(context=context) # Secure the connection
+    server.ehlo() # Can be omitted
+    server.login(sender_email, password)
+    
+    if float(change_per[:-1]) > 0:
+        news =f"""Day End Price:{final_price} IBM: 🔺{change_per} Headline: {headline}?. Brief: {Brief[:50]}"""
+    else :
+        news =f"""Day End Price:{final_price} IBM: 🔻{change_per}% Headline: {headline}?. Brief: {Brief[:50]}"""
+    # TODO: Send email here
+    server.sendmail(sender_email, "faisalmalikfr.99@gmail.com", msg=f"Subject:Stock Report \n\n {news}")
+except Exception as e:
+    # Print any error messages to stdout
+    print(e)
+finally:
+    server.quit()
